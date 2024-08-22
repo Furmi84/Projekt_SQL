f923f368704d0ffe9a3123a5722bf0a919fa48d5
Furmi84 <159477303+Furmi84@users.noreply.github.com>
2024-08-22 18:03:04 +0200

    Úprava skriptu

--- otazka_3_v2.sql
+++ otazka_3_v2.sql
@@ -1,5 +1,6 @@
--- otazka č.3
+-- otazka č.3 verze 2
 -- Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
+
 WITH calculated_changes AS (
     SELECT
         category_code,
@@ -27,9 +28,9 @@ SELECT
     price_one_piece,
     prev_avg_price_data,
     price_one_piece_minus_prev_avg_price_data,
-    percentage_change_per_year
-    -- ROUND(AVG(percentage_change_per_year), 2) AS avg_percentage_change_per_year
+    percentage_change_per_year,
+    ROUND(AVG(percentage_change_per_year), 2) AS avg_percentage_change_per_year
 FROM percentage_changes
-where prev_avg_price_data is not null
+WHERE prev_avg_price_data IS NOT NULL
 GROUP BY category_code,payroll_year
-order by percentage_change_per_year;
+ORDER BY percentage_change_per_year;
