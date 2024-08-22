f73eca57cd20103f44e537ef348e8e5cff181d13
Furmi84 <159477303+Furmi84@users.noreply.github.com>
2024-08-22 17:47:20 +0200

    Úprava skriptu

--- otazka_1.sql
+++ otazka_1.sql
@@ -1,31 +1,30 @@
 -- Otazka č.1
 -- Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 
-with callcuation_payroll as (
-select	
+WITH callcuation_payroll AS (
+SELECT	
 		industry_branch_code,
-		(select name
-			from czechia_payroll_industry_branch cpib 
-			where cpib.code = hpv.industry_branch_code
-		) as industry_branch_text,
+		(SELECT name
+			FROM czechia_payroll_industry_branch cpib 
+			WHERE cpib.code = hpv.industry_branch_code
+		) AS industry_branch_text,
 		payroll_year,
-		avg(payroll_value) as avg_payroll_value,	
-		LAG(avg(payroll_value)) OVER ( PARTITION BY industry_branch_code ORDER by payroll_year ) as avg_payroll_value_year_before,
-		round(avg(payroll_value) - LAG(avg(payroll_value)) OVER ( PARTITION BY industry_branch_code ORDER by payroll_year ),2) AS delta_payroll_per_year
-from t_martin_furmanek_project_SQL_primary_final hpv
-where industry_branch_code is not null
-group by  industry_branch_code, payroll_year
+		AVG(payroll_value) as avg_payroll_value,	
+		LAG(AVG(payroll_value)) OVER ( PARTITION BY industry_branch_code ORDER by payroll_year ) AS avg_payroll_value_year_before,
+		ROUND(AVG(payroll_value) - LAG(AVG(payroll_value)) OVER ( PARTITION BY industry_branch_code ORDER by payroll_year ),2) AS delta_payroll_per_year
+FROM t_martin_furmanek_project_SQL_primary_final hpv
+WHERE industry_branch_code IS NOT NULL
+GROUP BY industry_branch_code, payroll_year
 )
-select 
+SELECT
 	industry_branch_code,
 	industry_branch_text,
 	payroll_year,
 	avg_payroll_value,
 	avg_payroll_value_year_before,
 	delta_payroll_per_year
-from callcuation_payroll
-group by industry_branch_code,payroll_year;
-
+FROM callcuation_payroll
+GROUP BY industry_branch_code,payroll_year;
 
 
 
