3f4061bf5090ef4bc3aa17e0b73c558fe72f0766
Furmi84 <159477303+Furmi84@users.noreply.github.com>
2024-08-22 17:52:34 +0200

    Úprava skriptu

--- otazka_2.sql
+++ otazka_2.sql
@@ -1,12 +1,14 @@
 -- otazka č.2
-select 
+SELECT 
 	payroll_year,	
 	category_code,
-	price_name  ,	
-	round(avg(payroll_value)/price_one_piece) as amount_items_kg_l
-from t_martin_furmanek_project_SQL_primary_final hpv
-where	category_code in (114201, 111301)	
-		and (payroll_year = (SELECT MIN(payroll_year) FROM t_martin_furmanek_project_SQL_primary_final)
-		or payroll_year = (SELECT MAX(payroll_year) FROM t_martin_furmanek_project_SQL_primary_final))
-		and industry_branch_code is null
-group by  payroll_year, category_code ;
+	price_name  ,
+	payroll_value,
+	price_one_piece,
+	ROUND(AVG(payroll_value)/price_one_piece) as amount_items_kg_l
+FROM t_martin_furmanek_project_SQL_primary_final hpv
+WHERE	category_code IN (114201, 111301)	
+		AND (payroll_year = (SELECT MIN(payroll_year) FROM t_martin_furmanek_project_SQL_primary_final)
+		OR payroll_year = (SELECT MAX(payroll_year) FROM t_martin_furmanek_project_SQL_primary_final))
+		AND industry_branch_code IS NULL
+GROUP BY  payroll_year, category_code ;
